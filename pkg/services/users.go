3ccbba86a332ed4018f5ae0b0e7f146c5bf0a7a0
Filipe Andujar <filipeandujar@gmail.com>
2024-04-17 08:07:13 -0300

    feat: add teams and users services

--- pkg/services/users.go
+++ pkg/services/users.go
@@ -1,3 +1,80 @@
 package services
 
-// TODO: Implement the user service.
+import (
+	"context"
+
+	"github.com/fandujar/choregate/pkg/entities"
+	"github.com/fandujar/choregate/pkg/repositories"
+	"github.com/google/uuid"
+)
+
+type UserService struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) GetUser(ctx context.Context, id uuid.UUID) (*entities.User, error) {
+	user, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *UserService) GetUsers(ctx context.Context) ([]*entities.User, error) {
+	users, err := s.repo.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s *UserService) CreateUser(ctx context.Context, user *entities.User) error {
+	err := s.repo.Create(ctx, user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, user *entities.User) error {
+	err := s.repo.Update(ctx, user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	err := s.repo.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *UserService) GetUserByEmail(ctx context.Context, email string) (*entities.User, error) {
+	users, err := s.repo.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var user *entities.User
+
+	for _, u := range users {
+		if u.Email == email {
+			user = u
+			break
+		}
+	}
+
+	return user, nil
+}
