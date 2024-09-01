be2d439871102456707638f4b22a72197a586407
hcavarsan <hcavarsan@yahoo.com.br>
2024-09-01 01:45:41 -0300

    feat: remove old sql and add a initial Readme file
    feat: remove old sql and add a initial Readme file
    
    feat: remove old sql and add a initial Readme file

--- sql/tasks.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    namespace TEXT NOT NULL,
-    description TEXT,
-    timeout INTERVAL,
-    task_scope JSONB,
-    task_spec JSONB
-);
