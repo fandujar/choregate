dab120c99d0b906b4163eed7c5b4f8c262ce09ba
Filipe Andujar <filipeandujar@gmail.com>
2024-10-12 22:37:57 -0300

    feat: add error to auth hook

--- choregate-ui/src/hooks/Auth.tsx
+++ choregate-ui/src/hooks/Auth.tsx
@@ -90,4 +90,10 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     );
 }
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+    if (context === undefined) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+}
